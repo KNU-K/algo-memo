2dda1cc4f6667e18ea0f787cfc1934443ae45d71
taehyun <126179088+KNU-K@users.noreply.github.com>
2024-08-03 21:59:01 +0900

    Delete backend directory

--- backend/src/main/java/com/example/backend/repository/IncorrectAlgorithmRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.domain.IncorrectAlgorithm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncorrectAlgorithmRepository extends JpaRepository<IncorrectAlgorithm, Long> {
-
-}
