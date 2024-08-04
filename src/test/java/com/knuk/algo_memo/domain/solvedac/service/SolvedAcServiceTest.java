63a4f0bcb1a9a9f93cc819ca8e82912308cf8c47
KNU-K <knukang334@gmail.com>
2024-08-05 08:28:47 +0900

    test : 테스트 추가

--- src/test/java/com/knuk/algo_memo/domain/solvedac/service/SolvedAcServiceTest.java
+++ src/test/java/com/knuk/algo_memo/domain/solvedac/service/SolvedAcServiceTest.java
@@ -4,24 +4,42 @@ import com.knuk.algo_memo.domain.solvedac.dto.ProblemDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
+@DisplayName("SolvedAcService에 대한 테스트")
 @SpringBootTest
 public class SolvedAcServiceTest {
 
     @Autowired
     SolvedAcService solvedAcService;
 
-    @DisplayName("SolvedAc API 가지고 오기")
-    @Test
-    void shouldToGetTheProblemInfo(){
-        ProblemDTO res = solvedAcService.getProblemInfo(1000);
+    @DisplayName("SolvedAc에 문제를 요청 시")
+    @Nested
+    class WhenRequestingProblem {
 
-        Assertions.assertEquals(res.getId(), 1000);
-        Assertions.assertEquals(res.getTitle(), "A+B");
-        Assertions.assertEquals(res.getLevel(), 1);
+        @DisplayName("있는 문제인 경우")
+        @Test
+        void shouldGetProblemInfoIfExists() {
+
+            ProblemDTO res = solvedAcService.searchProblemById(1000);
+
+            Assertions.assertNotNull(res);
+            Assertions.assertEquals(res.getId(), 1000);
+            Assertions.assertEquals(res.getTitle(), "A+B");
+            Assertions.assertEquals(res.getLevel(), 1);
+        }
+
+        @DisplayName("없는 문제인 경우 (예외 발생)")
+        @Test
+        void shouldHandleNonexistentProblem() {
+            RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+                solvedAcService.searchProblemById(1);
+            });
+
+            Assertions.assertEquals("Problem with ID 1 not found", exception.getMessage());
+        }
     }
-
 }
