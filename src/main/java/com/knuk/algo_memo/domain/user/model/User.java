2d5c5236e76352e219734100d2a58e7846170bab
KNU-K <knukang334@gmail.com>
2024-08-05 21:39:35 +0900

    add : user entity 추가

--- src/main/java/com/knuk/algo_memo/domain/user/model/User.java
+++ src/main/java/com/knuk/algo_memo/domain/user/model/User.java
@@ -1,23 +1,44 @@
 package com.knuk.algo_memo.domain.user.model;
 
+import com.knuk.algo_memo.global.model.BaseEntityWithUpdate;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
 
-@Entity
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity(name = "users")
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@SQLRestriction("deleted_at is null")
 @AllArgsConstructor
-public class User {
+@ToString
+public class User extends BaseEntityWithUpdate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String nickName;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
