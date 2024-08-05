7244c9699fefd8a4c961a4c9acaccecfa3428ad0
KNU-K <knukang334@gmail.com>
2024-08-05 21:40:01 +0900

    add : global BaseEntity 구성 (Create, Update)

--- src/main/java/com/knuk/algo_memo/global/model/BaseEntityWithUpdate.java
+++ src/main/java/com/knuk/algo_memo/global/model/BaseEntityWithUpdate.java
@@ -1,2 +1,25 @@
-package com.knuk.algo_memo.global.model;public class BaseEntityWithUpdate {
+package com.knuk.algo_memo.global.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.*;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntityWithUpdate extends BaseEntity {
+
+    @NotNull
+    @LastModifiedDate
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime updatedAt;
 }
